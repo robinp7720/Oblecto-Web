9bc84c37ebf4b1bae1eccf8b93526796901d714d
Robin <robin@decker.cx>
2018-12-07 17:48:54 +0100

    Add preliminary user management

---
+++ src/components/modals/userAdd.vue
@@ -0,0 +1,79 @@
+<template>
+  <modal name="modifyShow" @before-open="beforeOpen">
+    <div class="container">
+      <div class="heading">
+      <h3>Add user</h3>
+      </div>
+      <div class="body">
+
+      </div>
+      <div class="footer">
+        <button class="success">Add user</button>
+      </div>
+    </div>
+  </modal>
+</template>
+
+<script>
+  export default {
+    name: 'show-modify',
+    data () {
+      return {
+        show: {}
+      }
+    },
+    methods: {
+      beforeOpen (event) {
+        this.show = event.params.show
+      },
+      indexShow: function () {
+        this.axios.get('/series/' + this.show.id + '/index')
+          .then(response => {
+            this.$notify({
+              group: 'system',
+              title: 'Library update requested successfully for ' + this.show.seriesName,
+              text: 'A library index update has been started',
+              type: 'warning'
+            })
+          })
+          .catch(e => {})
+      }
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+
+  h3
+    width: 100%
+    color: white
+    margin: 0
+    margin-bottom: 10px
+    padding: 20px
+
+    background-color: rgba(0,0,0,0.3)
+
+    box-shadow: 0px 0px 5px 2px rgba(darken(darken(#696060,17) + #000000,6), 0.75)
+
+  ul
+    list-style: none
+    padding: 0
+    margin: 0
+    li
+      a
+        display: inline-block
+        width: 100%
+        padding: 10px 20px
+        color: white
+        text-decoration: none
+
+        -webkit-transition: background-color 0.1s
+        -moz-transition: background-color 0.1s
+        -ms-transition: background-color 0.1s
+        -o-transition: background-color 0.1s
+        transition: background-color 0.1s
+      a:hover
+        background-color: rgba(0,0,0,0.1)
+        box-shadow: 0px 0px 2px 2px rgba(darken(darken(#696060,17) + #000000,6), 0.2)
+
+</style>
---
+++ src/components/settings/UserManager.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="UserManager">
+    <table>
+      <thead>
+      <tr>
+        <th>User ID</th>
+        <th>Name</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Group</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
+      <tbody>
+      <user-entry v-for="(user, index) in users"
+            v-bind:user="user"
+            v-bind:key="user.id"
+      ></user-entry>
+      </tbody>
+    </table>
+  <a href="" class="button">Add users</a>
+  </div>
+</template>
+
+<script>
+  import userEntry from '@/components/settings/UserManagement/userEntry'
+
+  export default {
+    name: 'user-manager',
+    components: {userEntry},
+    data () {
+      return {
+        users: {}
+      }
+    },
+    async created () {
+      this.users = (await this.axios.get(`/users`)).data
+    }
+  }
+</script>
+
+<style scoped lang="sass">
+  table
+    background: #696060
+    box-shadow: 0px 0px 5px 2px rgba(darken(darken(#696060,17) + #000000,6), 0.75)
+
+    width: 100%
+
+  thead
+    background-color: #444042
+
+    th
+      padding: 5px
+
+      margin: 0
+      border: 0
+
+      outline: 0
+
+</style>
