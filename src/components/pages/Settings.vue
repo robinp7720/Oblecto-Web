337e926beabf6313b76e30ebf15bc879fb65793d
Robin <robin@decker.cx>
2019-01-03 13:11:59 +0100

    Improve wrapping of buttons on smaller devices

--- src/components/pages/Settings.vue
+++ src/components/pages/Settings.vue
@@ -88,13 +88,21 @@
 
 hr
   border-color: rgba(0,0,0,0.5)
+  width: 100%
+  display: block
+  float: left
 
 button
   background-color: rgba(0,0,0,0.5)
   border: rgba(0,0,0,0.8) 1px solid
   color: rgba(255,255,255,0.5)
 
+  display: block
+  float: left
+
   padding: 10px
+  margin: 2px
+
   -webkit-border-radius: 3px
   -moz-border-radius: 3px
   border-radius: 3px
