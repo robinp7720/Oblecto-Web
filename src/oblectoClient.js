1d201956d604a82dbd51b76dcae04acd29c04183
Robin <robin@decker.cx>
2020-10-12 21:22:27 +0200

    Add support for https servers

--- src/oblectoClient.js
+++ src/oblectoClient.js
@@ -1,7 +1,12 @@
 import OblectoClient from 'oblectoclient'
 
-let host = OBLECTO_HOST ||
-  window.location.protocol + '//' + window.location.hostname + ':' + (window.location.port || '80')
+let host = window.location.protocol + '//' + window.location.hostname
+
+if (window.location.port) {
+  host = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port
+}
+
+if (OBLECTO_HOST) host = OBLECTO_HOST
 
 console.log(OBLECTO_HOST)
 
