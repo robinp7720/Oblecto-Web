8b43c9cd0c80fe138422ba060e2da70de2f5aa9c
Zeyphros <robin@decker.cx>
2020-11-24 14:18:50 +0100

    Set the relevant mediaSession objects when an episode is paying played

--- src/components/playBar.vue
+++ src/components/playBar.vue
@@ -159,7 +159,7 @@
       iconCog () {
         return faCog
       },
-      ...mapState(['playing', 'autoplay'])
+      ...mapState(['playing', 'autoplay', 'host'])
     },
     methods: {
       viewShow: function () {
@@ -321,6 +321,8 @@
         this.nextepisode = null
 
         if (this.playing.entity === undefined) {
+          navigator.mediaSession.playbackState = 'none'
+
           return
         }
 
@@ -347,11 +349,36 @@
 
         this.setURL()
 
+        // Set the mediaSession environment
+        if ('mediaSession' in navigator) {
+          let imageURL = ''
+
+          if (this.playing.type === 'episode') {
+            imageURL = this.host + '/episode/' + this.playing.entity.id + '/banner'
+          }
+
+          console.log(imageURL)
+
+          // eslint-disable-next-line no-undef
+          navigator.mediaSession.metadata = new MediaMetadata({
+            title: this.playing.title,
+            album: this.playing.entity.Series.seriesName,
+            artwork: [
+              { src: imageURL }
+            ]
+          })
+
+          navigator.mediaSession.setActionHandler('nexttrack', this.playNext)
+          navigator.mediaSession.setActionHandler('stop', this.stopPlaying)
+        }
+
         if (this.playing.type === 'episode') {
           this.nextepisode = (await this.axios.get(`/episode/${this.playing.entity.id}/next`)).data
         }
       },
       paused: async function (newState, oldState) {
+        navigator.mediaSession.playbackState = newState ? 'paused' : 'playing'
+        console.log('status', navigator.mediaSession.playbackState)
         if (newState) {
           return this.player.pause()
         }
